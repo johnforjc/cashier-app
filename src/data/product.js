6b43ba8b0bfe294aea1f3aa354318109075f5e94
unknown <animeloverz072@gmail.com>
2021-11-22 19:44:22 +0700

    tambah produk ke list kasir selesai

---
+++ src/data/product.js
@@ -0,0 +1,194 @@
+const product = [
+  {
+    id: 1,
+    name: "Sticky Mucus",
+    price: 7000,
+  },
+  {
+    id: 2,
+    name: "Madragora Leaf",
+    price: 7500,
+  },
+  {
+    id: 3,
+    name: "Chrysalis",
+    price: 5500,
+  },
+  {
+    id: 4,
+    name: "Small Meat Piece",
+    price: 2500,
+  },
+  {
+    id: 5,
+    name: "Egg",
+    price: 6500,
+  },
+  {
+    id: 6,
+    name: "Large Meat Piece",
+    price: 6000,
+  },
+  {
+    id: 7,
+    name: "Mushroom Spore",
+    price: 4500,
+  },
+  {
+    id: 8,
+    name: "Tentacle",
+    price: 1500,
+  },
+  {
+    id: 9,
+    name: "Sticky Webfoot",
+    price: 5000,
+  },
+  {
+    id: 10,
+    name: "Grasshopper Leg",
+    price: 6500,
+  },
+  {
+    id: 11,
+    name: "Chonchon Leg",
+    price: 3500,
+  },
+  {
+    id: 12,
+    name: "Nectar",
+    price: 4500,
+  },
+  {
+    id: 13,
+    name: "Clover",
+    price: 6000,
+  },
+  {
+    id: 14,
+    name: "Stem",
+    price: 2000,
+  },
+  {
+    id: 15,
+    name: "Silk Thread",
+    price: 6000,
+  },
+  {
+    id: 16,
+    name: "Feather",
+    price: 5500,
+  },
+  {
+    id: 17,
+    name: "Piece of Egg Shell",
+    price: 5000,
+  },
+  {
+    id: 18,
+    name: "Animal Fang",
+    price: 5000,
+  },
+  {
+    id: 19,
+    name: "Mushroom Cap",
+    price: 2500,
+  },
+  {
+    id: 20,
+    name: "Single Cell",
+    price: 2000,
+  },
+  {
+    id: 21,
+    name: "Smooth Leather",
+    price: 7000,
+  },
+  {
+    id: 22,
+    name: "Broken Violin String",
+    price: 7000,
+  },
+  {
+    id: 23,
+    name: "Bat Ear",
+    price: 6500,
+  },
+  {
+    id: 24,
+    name: "Chonchon Eye",
+    price: 4000,
+  },
+  {
+    id: 25,
+    name: "Thief Bug Slime",
+    price: 4000,
+  },
+  {
+    id: 26,
+    name: "Small Flower",
+    price: 4500,
+  },
+  {
+    id: 27,
+    name: "Thin Feelers",
+    price: 2000,
+  },
+  {
+    id: 28,
+    name: "Bat Wing",
+    price: 6000,
+  },
+  {
+    id: 29,
+    name: "Insect Wing",
+    price: 4500,
+  },
+  {
+    id: 30,
+    name: "Bat Tooth",
+    price: 4000,
+  },
+  {
+    id: 31,
+    name: "Thief Bug Tooth",
+    price: 5500,
+  },
+  {
+    id: 32,
+    name: "Drifting Crystal Shard",
+    price: 6000,
+  },
+  {
+    id: 33,
+    name: "Hinalle",
+    price: 2500,
+  },
+  {
+    id: 34,
+    name: "Strong Slime",
+    price: 3500,
+  },
+  {
+    id: 35,
+    name: "Chonchon Shell",
+    price: 7000,
+  },
+  {
+    id: 36,
+    name: "Thief Bug Shell",
+    price: 6500,
+  },
+  {
+    id: 37,
+    name: "Down Fur",
+    price: 6500,
+  },
+  {
+    id: 38,
+    name: "Insect Peeling",
+    price: 6500,
+  },
+];
+
+export default product;
--- src/pages/cashier-page/Cashier.js
+++ src/pages/cashier-page/Cashier.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { useState } from "react";
+
+import product from "../../data/product";
 import "./Cashier.css";
 
 const Cashier = () => {
+  const [inputValue, setinputValue] = useState("");
+  const [suggestProduct, setSuggestProduct] = useState([]);
+  const [productSelected, setProductSelected] = useState([]);
+
+  const getSuggestItem = (inputText) => {
+    if (inputText.length > 0) {
+      const suggestItem = product.filter((item) => item.name.toLowerCase().includes(inputText.toLowerCase()));
+
+      setSuggestProduct(suggestItem);
+    } else {
+      setSuggestProduct([]);
+    }
+  };
+
+  const inputChangeHandler = (event) => {
+    let currentValue = event.target.value;
+    setinputValue(currentValue);
+
+    getSuggestItem(currentValue);
+  };
+
+  const selectedValue = (event) => {
+    let elementTarget = event.target.innerText;
+
+    setinputValue(elementTarget);
+    setSuggestProduct([]);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    let choosenProduct = inputValue;
+
+    let searchItem = product.filter((item) => item.name.toLowerCase() === choosenProduct.toLowerCase());
+
+    if (searchItem.length === 1) {
+      const quantity = 1;
+      const newItem = { quantity, ...searchItem[0] };
+
+      setProductSelected([...productSelected, newItem]);
+    } else {
+      console.log("Not Found");
+    }
+
+    setinputValue("");
+  };
+
   const cetakList = () => {
     let list = document.querySelectorAll(".list-item");
 
@@ -13,6 +62,23 @@ const Cashier = () => {
 
   return (
     <div className="cashier">
+      <div className="search-item">
+        <form action="" className="form-control" autoComplete="off" onSubmit={submitHandler}>
+          <div className="form-input">
+            <input type="text" placeholder="Search Item" id="item-name" onChange={inputChangeHandler} value={inputValue} autoComplete="off" />
+            <input type="submit" value="Submit" id="submit-btn" />
+          </div>
+          {suggestProduct.length > 0 && (
+            <div className="suggested-item">
+              {suggestProduct.map((item) => (
+                <div className="suggested-list" key={item.id} onClick={selectedValue}>
+                  {item.name}
+                </div>
+              ))}
+            </div>
+          )}
+        </form>
+      </div>
       <div className="cashier-table">
         <div className="list-header">
           <div className="name">Product</div>
@@ -22,66 +88,14 @@ const Cashier = () => {
         </div>
 
         <div className="cashier-table-content">
-          <div className="list-item">
-            <div className="name">Yoyo Tail</div>
-            <div className="qty">3</div>
-            <div className="price">4500</div>
-            <div className="subtotal">13500</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">3000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name">Monkey Tail</div>
-            <div className="qty">2</div>
-            <div className="price">1500</div>
-            <div className="subtotal">10003000</div>
-          </div>
-          <div className="list-item">
-            <div className="name"></div>
-            <div className="qty"></div>
-            <div className="price"></div>
-            <div className="subtotal"></div>
-          </div>
-          <div className="list-item">
-            <div className="name"></div>
-            <div className="qty"></div>
-            <div className="price"></div>
-            <div className="subtotal"></div>
-          </div>
+          {productSelected.map((item, index) => (
+            <div className="list-item" key={index}>
+              <div className="name">{item.name}</div>
+              <div className="qty">{item.quantity}</div>
+              <div className="price">{item.price}</div>
+              <div className="subtotal">{item.quantity * item.price}</div>
+            </div>
+          ))}
         </div>
 
         <div className="list-total">
